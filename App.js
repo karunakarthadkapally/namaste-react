51dcad0850ad77a3a1df1326a56d7362da2ae120
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-18 17:33:07 +0530

    adding multiple childs as siblings

--- App.js
+++ App.js
@@ -1,29 +1,26 @@
 /**
  *<div id="parent">
- *    <div id="child">
+ *    <div id="child1">
+ *       <h1> Heading 1</h1>
+ *       <h2> Heading 2</h2>
+ *    </div>
+ *    <div id="chil2">
  *       <h1> Heading 1</h1>
  *       <h2> Heading 2</h2>
  *    </div>
  *</div>
  *
  */
-const parent =React.createElement(
-    "div",{id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},[
-            React.createElement(
-                "h1",
-                {},
-                "Heading 1"
-            ),
-            React.createElement(
-                "h1",
-                {},
-                "Heading 2"
-            )
-        ])
-);
+const parent =React.createElement("div", {id:"parent"},[   
+        React.createElement("div", {id:"child1"},[
+                React.createElement("h1", {}, "Heading 1"),
+                React.createElement("h1", {}, "Heading 2")
+            ]),
+        React.createElement("div",{id:"child2"},[
+                React.createElement("h1", {}, "Heading 1"),
+                React.createElement("h1", {}, "Heading 2")
+            ])
+    ]);
 
 
 const header = React.createElement(
