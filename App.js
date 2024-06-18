69d906b5b870cb10251d7581e91b205dcdf973b7
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-18 16:59:56 +0530

    added attributes to the header React object

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
 
-const header = React.createElement("h1",{},"Hello World using React!");
+const header = React.createElement("h1",{id:"header", xyz:"abc"},"Hello World using React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
