8974992c075c8611ab8243ae7d4ddc437ec319e1
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-19 14:01:12 +0530

    we can use {} to have anythnig inside anything

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elem = <span>React Element : </span>;
+
+// React element inside element
 const title = (
   <h1 className="title">
+    {elem}
     Namaste React Using JSX with React element inside component
   </h1>
 );
+
+// we can use component also inside element by using {}
 
 // putting React element in side component using curlie brackets
 const HeadingComponent = () => (
