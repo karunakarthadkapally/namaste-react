55f09795e0571cf824ea7c2f705565663825c52f
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-19 08:24:50 +0530

    REmoved CDN links and added parcel bundler to from npm to bundle and packege

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
-/**
- *<div id="parent">
- *    <div id="child1">
- *       <h1> Heading 1</h1>
- *       <h2> Heading 2</h2>
- *    </div>
- *    <div id="chil2">
- *       <h1> Heading 1</h1>
- *       <h2> Heading 2</h2>
- *    </div>
- *</div>
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent =React.createElement("div", {id:"parent"},[   
         React.createElement("div", {id:"child1"},[
                 React.createElement("h1", {}, "Heading 1"),
@@ -23,12 +13,7 @@ const parent =React.createElement("div", {id:"parent"},[
     ]);
 
 
-const header = React.createElement("h1",
-    {id:"header", xyz:"abc"},
-    "Hello World using React!"
-);
-
-console.log(header);  //Object
+console.log(parent);  //Object
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 
+
+#parcel
+- Dev Build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistence Hashing
+- Code Splitting
+- Differential Bundling - supports older versions of browsers
+- Diagnostics
+- Error Handling
+- HTTPS
+- Tree Shaking - removes unused code
+- Different dev and prod bundles
