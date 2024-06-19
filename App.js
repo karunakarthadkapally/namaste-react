bfd53e46c54469fc6367dfcac2a907479fcb7aa6
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-19 09:14:04 +0530

    best practice to have Not Rendered header by default

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent =React.createElement("div", {id:"parent"},[   
-        React.createElement("div", {id:"child1"},[
-                React.createElement("h1", {}, "Heading 1"),
-                React.createElement("h1", {}, "Heading 2")
-            ]),
-        React.createElement("div",{id:"child2"},[
-                React.createElement("h1", {}, "Heading 1"),
-                React.createElement("h1", {}, "Heading 2")
-            ])
-    ]);
+//React.createElement  ==> Object ==> HTMLElement(render)
 
+const header = React.createElement(
+    "h1",
+    {id:"header"},
+    "Namaste React!"
+);
 
-console.log(parent);  //Object
-
+console.log(header);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(header);
