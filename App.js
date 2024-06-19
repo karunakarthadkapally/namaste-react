82c7bbe43ad52f960d35e3b64095b8d5aa97a8a3
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-19 13:53:13 +0530

    React element inside React component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 className="title">Namaste React Using JSX with component composition</h1>
+const title = (
+  <h1 className="title">
+    Namaste React Using JSX with React element inside component
+  </h1>
 );
 
-// component composition  ==> composing two component in one another is called component composition
+// putting React element in side component using curlie brackets
 const HeadingComponent = () => (
   <div id="container">
-    <Title />
+    {title}
     <h1 className="heading">Namaste React Functional component.</h1>
   </div>
 );
