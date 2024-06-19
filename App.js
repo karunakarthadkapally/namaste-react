680157bc83e3e38a911e5a38134499e691128ea3
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-19 13:14:41 +0530

    JSX can be nested

--- App.js
+++ App.js
@@ -14,8 +14,15 @@ const HeadingComponent1 = () => {
 // In arrow functions in javascript, we can skip the return key if we want to return only one line. like below
 // if we want to return more line we can use () as shown in below.
 
-const HeadingComponent = () => (
+const HeadingComponent2 = () => (
   <h1>Namaste React using Functional component.</h1>
+);
+
+// JSX can be nested
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="header">Namaste React using Functional component.</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
