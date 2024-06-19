e6dca4c55140888682c00ca9fa9d3d7352187992
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-19 13:21:23 +0530

    difference between React element and Functional component

--- App.js
+++ App.js
@@ -6,26 +6,21 @@ const header = (
   <h1 className="header">Namaste React Using JSX with multiple line</h1>
 );
 
-// React Functional component.
-const HeadingComponent1 = () => {
-  return <h1>Namaste React using Functional component.</h1>;
-};
+// if we add function to React element for te above it can be React component.
 
-// In arrow functions in javascript, we can skip the return key if we want to return only one line. like below
-// if we want to return more line we can use () as shown in below.
-
-const HeadingComponent2 = () => (
-  <h1>Namaste React using Functional component.</h1>
+const Title = () => (
+  <h1 className="title">Namaste React Using JSX with multiple line</h1>
 );
 
 // JSX can be nested
 const HeadingComponent = () => (
   <div id="container">
-    <h1 className="header">Namaste React using Functional component.</h1>
+    <h1 className="heading">Namaste React using Functional component.</h1>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// we should use < component />  while render the react components
-root.render(<HeadingComponent />);
+// we should use < component />  while render the react components but for REact element we can us directly
+//root.render(header);  //for React element
+root.render(<HeadingComponent />); // for functional component
