a98447947ca306bae923c230aee34ba221909e91
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-20 08:52:56 +0530

    Initial commit for Food Delivery App

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const Title = () => (
-  <h1 className="title">
-    Namaste React Using JSX with React Component inside another component
-  </h1>
-);
-
-// putting React components in side another component 3 different ways we can call another component.
-const HeadingComponent = () => (
-  <div id="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1 className="heading">Namaste React Functional component.</h1>
-  </div>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<HeadingComponent />);
+/**
+ * HEADER
+ * BOBY
+ * FOOTER
+ *
+ */
