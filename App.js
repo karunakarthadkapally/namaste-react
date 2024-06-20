31ef60a1a18b018b987abe06fe388ca1e823fd45
Karunakar Thadkapally <karunakar.drm@gmail.com>
2024-06-20 09:38:57 +0530

    Basic code written in containers

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /**
  *  HEADER
  *   - Logo
@@ -13,3 +16,39 @@
  *   - Contact
  *
  */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container"></div>
+      <div className="nav-items"></div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div clasaName="search-container"></div>
+      <div className="res-container"></div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <div className="footer"></div>;
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
